ae82dff6e255598212bc229d01c02c35b81da21a
Umur Can <>
2024-01-23 21:07:16 +0300

    updated fastlane

--- setup_fastlane.sh
+++ setup_fastlane.sh
@@ -4,40 +4,41 @@ FASTLANE_P12_FILE=$2
 FASTLANE_PROVISIONING_PROFILE_PATH=$3
 echo "Build Path: $BUILD_PATH"
 
-# Echo the paths of the P12 file and provisioning profile
-echo "P12 File Path: $FASTLANE_P12_FILE"
-echo "Provisioning Profile Path: $FASTLANE_PROVISIONING_PROFILE_PATH"
+# Import the P12 file into the keychain
+echo "Importing P12 File: $FASTLANE_P12_FILE"
+security import "$FASTLANE_P12_FILE" -k ~/Library/Keychains/login.keychain-db -P [P12_PASSWORD] -T /usr/bin/codesign
 
- FASTLANE_DIR="$BUILD_PATH/fastlane"
+# Verify the import of P12
+echo "Verifying P12 import:"
+security find-identity -p codesigning -v
 
- # Import the P12 file into the keychain
- echo "Importing P12 File: $FASTLANE_P12_FILE"
- security import "$FASTLANE_P12_FILE" -k ~/Library/Keychains/login.keychain-db -P 123456 -T /usr/bin/codesign
+# Copy the provisioning profile to the correct directory
+echo "Copying provisioning profile: $FASTLANE_PROVISIONING_PROFILE_PATH"
+cp "$FASTLANE_PROVISIONING_PROFILE_PATH" ~/Library/MobileDevice/Provisioning\ Profiles/
 
- # Verify the import of P12
- echo "Verifying P12 import:"
- security find-identity -p codesigning -v
+# Verify the copy of the provisioning profile
+echo "Copied provisioning profiles:"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
- # Copy the provisioning profile to the correct directory
- echo "Copying provisioning profile: $FASTLANE_PROVISIONING_PROFILE_PATH"
- cp "$FASTLANE_PROVISIONING_PROFILE_PATH" ~/Library/MobileDevice/Provisioning\ Profiles/
+# Set environment variables for Fastlane to use specific certificate and provisioning profile
+export MATCH_CERTIFICATE="$FASTLANE_P12_FILE"
+export MATCH_PROVISIONING_PROFILE_PATH="$FASTLANE_PROVISIONING_PROFILE_PATH"
 
- # Verify the copy of the provisioning profile
- echo "Copied provisioning profiles:"
- ls ~/Library/MobileDevice/Provisioning\ Profiles/
+FASTLANE_DIR="$BUILD_PATH/fastlane"
 
- # Setup Fastlane directory and Fastfile if not exists
- if [ ! -d "$FASTLANE_DIR" ]; then
-     mkdir -p "$FASTLANE_DIR"
-     touch "$FASTLANE_DIR/Fastfile"
+# Setup Fastlane directory and Fastfile if not exists
+if [ ! -d "$FASTLANE_DIR" ]; then
+    mkdir -p "$FASTLANE_DIR"
+    touch "$FASTLANE_DIR/Fastfile"
 
-     echo "default_platform(:ios)" >> "$FASTLANE_DIR/Fastfile"
-     echo "" >> "$FASTLANE_DIR/Fastfile"
-     echo "platform :ios do" >> "$FASTLANE_DIR/Fastfile"
-     echo "  desc 'Deploy the app to TestFlight/App Store'" >> "$FASTLANE_DIR/Fastfile"
-     echo "  lane :deploy do" >> "$FASTLANE_DIR/Fastfile"
-     echo "    gym(export_method: 'app-store', project: 'Unity-iPhone.xcodeproj', scheme: 'Unity-iPhone', output_directory: 'builds/')" >> "$FASTLANE_DIR/Fastfile"
-     echo "    upload_to_app_store(skip_metadata: true, skip_screenshots: true, skip_binary_upload: false)" >> "$FASTLANE_DIR/Fastfile"
-     echo "  end" >> "$FASTLANE_DIR/Fastfile"
-     echo "end" >> "$FASTLANE_DIR/Fastfile"
- fi
+    echo "default_platform(:ios)" >> "$FASTLANE_DIR/Fastfile"
+    echo "" >> "$FASTLANE_DIR/Fastfile"
+    echo "platform :ios do" >> "$FASTLANE_DIR/Fastfile"
+    echo "  desc 'Deploy the app to TestFlight/App Store'" >> "$FASTLANE_DIR/Fastfile"
+    echo "  lane :deploy do" >> "$FASTLANE_DIR/Fastfile"
+    echo "    gym(export_method: 'app-store', project: 'Unity-iPhone.xcodeproj', scheme: 'Unity-iPhone', output_directory: 'builds/'," >> "$FASTLANE_DIR/Fastfile"
+    echo "        signing_identity: 'iPhone Distribution', export_options: { provisioningProfiles: { 'com.reboot.flipEmAll': 'flip_Em_All' } })" >> "$FASTLANE_DIR/Fastfile"
+    echo "    upload_to_app_store(skip_metadata: true, skip_screenshots: true, skip_binary_upload: false)" >> "$FASTLANE_DIR/Fastfile"
+    echo "  end" >> "$FASTLANE_DIR/Fastfile"
+    echo "end" >> "$FASTLANE_DIR/Fastfile"
+fi
